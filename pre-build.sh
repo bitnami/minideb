c772bace4d928571ea4922ca71c3f7c78d151ca8
Juan Jose Medina <juanjo@bitnami.com>
2021-02-22 12:58:05 +0100

    Make sure gcloud gets installed (#101)
    * Make sure gcloud gets installed

--- pre-build.sh
+++ pre-build.sh
@@ -10,4 +10,10 @@ if [[ ! -f /etc/debian_version ]]; then
 fi
 
 apt-get update
-apt-get install -y debootstrap debian-archive-keyring jq dpkg-dev
+apt-get install -y debootstrap debian-archive-keyring jq dpkg-dev gnupg apt-transport-https ca-certificates
+
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+apt-get update
+apt-get install -y google-cloud-sdk
