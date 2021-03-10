98de255d46e1406f95fe09de458f35d0b2730ffd
Juan Jose Medina <juanjo@bitnami.com>
2021-03-10 16:37:37 +0100

    Migrate to GitHub actions (#103)

--- pre-build.sh
+++ pre-build.sh
@@ -10,10 +10,16 @@ if [[ ! -f /etc/debian_version ]]; then
 fi
 
 apt-get update
-apt-get install -y debootstrap debian-archive-keyring jq dpkg-dev gnupg apt-transport-https ca-certificates
+apt-get install -y debootstrap debian-archive-keyring jq dpkg-dev gnupg apt-transport-https ca-certificates curl gpg
 
-echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+if ! command -v gcloud &> /dev/null
+then
+    echo "Installing gcloud"
+    echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-apt-get update
-apt-get install -y google-cloud-sdk
+    apt-get update
+    apt-get install -y google-cloud-sdk
+else
+    echo "gcloud is installed"
+fi
