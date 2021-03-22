59c31e40dd1c79930aaa6f95ba0e7744d68d8366
Juan Jose Medina <juanjo@bitnami.com>
2021-03-22 16:27:31 +0100

    Install gcloud dependency from github workflow (#108)

--- pre-build.sh
+++ pre-build.sh
@@ -12,14 +12,3 @@ fi
 apt-get update
 apt-get install -y debootstrap debian-archive-keyring jq dpkg-dev gnupg apt-transport-https ca-certificates curl gpg
 
-if ! command -v gcloud &> /dev/null
-then
-    echo "Installing gcloud"
-    echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-
-    apt-get update
-    apt-get install -y google-cloud-sdk
-else
-    echo "gcloud is installed"
-fi
